fd874bdaef51828ec9d883c750ca771a5de29b9b
AskNilesh <rathodnilsrk@gmail.com>
2023-04-14 22:23:11 +0530

    First commit

--- Length Conversion/ContentView.swift
+++ Length Conversion/ContentView.swift
@@ -7,15 +7,103 @@
 
 import SwiftUI
 
+enum LengthUnit: String, CaseIterable {
+    case METER = "Meters"
+    case KM = "KM"
+    case FEET = "Feet"
+    case YARDS = "Yards"
+    case MILES = "Miles"
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    
+    @State var inputConversionType = LengthUnit.METER
+    @State var outputConversionType = LengthUnit.KM
+    @State var inputNumber = 0.0
+    
+    @FocusState var inputFocus : Bool
+    
+    var convesrionResult : Double {
+        
+        let inputlengthInMeters: Double
+        let result: Double
+        
+        switch inputConversionType {
+        case .METER:
+            inputlengthInMeters = inputNumber
+        case .KM:
+            inputlengthInMeters = (inputNumber ) * 1000
+        case .FEET:
+            inputlengthInMeters = (inputNumber ) / 3.2808399
+        case .YARDS:
+            inputlengthInMeters = (inputNumber ) / 1.094
+        case .MILES:
+            inputlengthInMeters = (inputNumber) * 1609
         }
-        .padding()
+        
+        switch outputConversionType {
+        case .METER:
+            result = inputlengthInMeters
+        case .KM:
+            result = inputlengthInMeters / 1000
+        case .FEET:
+            result = inputlengthInMeters * 3.2808399
+        case .YARDS:
+            result = inputlengthInMeters * 1.094
+        case .MILES:
+            result = inputlengthInMeters / 1609
+        }
+        return result
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Section {
+                    Picker("Select input type", selection: $inputConversionType){
+                        ForEach(LengthUnit.allCases, id: \.self){
+                            Text($0.rawValue)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("Select the input type of conversion")
+                }
+                
+                Section {
+                    TextField("Enter Number", value: $inputNumber, format:.number)
+                        .keyboardType(.numberPad)
+                        .focused($inputFocus)
+                }
+                
+                Section {
+                    Picker("Select output type", selection: $outputConversionType){
+                        ForEach(LengthUnit.allCases, id: \.self){
+                            Text($0.rawValue)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("Select the output type of conversion")
+                }
+                
+                Section {
+                    Text(convesrionResult, format :.number)
+                } header: {
+                    Text("Select the output type of conversion")
+                }
+            }
+            .navigationTitle("Unit Conversions")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Done"){
+                        inputFocus = false
+                    }
+                }
+            }
+            
+        }
     }
 }
 
